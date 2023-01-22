bbd4b0026da2039b21d7a20536b562f03155c445
Klap.ir <mohammadrezaasiyaban2286@gmail.com>
2023-01-22 19:32:44 +0330

    for

---
+++ src/bakhsh_pazir_5.java
@@ -0,0 +1,9 @@
+public class bakhsh_pazir_5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/fibonachi.java
@@ -0,0 +1,4 @@
+public class fibonachi {
+    public static void main(String[] args) {
+    }
+}
